ad9adc6e737d63181023066f90f9bcd346621906
NathanCioppa <112644713+NathanCioppa@users.noreply.github.com>
2022-09-28 17:50:05 -0400

    paddles move correctly
    paddles can move at same time
    ball bounces around

--- index.js
+++ index.js
@@ -8,27 +8,67 @@ let c = canvas.getContext('2d')
 
 let bluePaddleY = 0
 let redPaddleY = 0
+let blueUp = false
+let blueDown = false
+let redUp = false
+let redDown = false
 
 addEventListener('keydown', (event) => {
     keyName = event.key
     
-    if (keyName === 'w' && bluePaddleY > 0) {
-        bluePaddleY += -2
+    if (keyName === 'w') {
+        blueDown = true
     }
-    if (keyName === 's' && bluePaddleY < 46) {
-        bluePaddleY += 2
+    if (keyName === 's') {
+        blueUp = true
     }
-    if (keyName === 'ArrowUp' && redPaddleY > 0) {
-        redPaddleY += -2
+    if (keyName === 'ArrowUp') {
+        redDown = true
     }
-    if (keyName === 'ArrowDown' && redPaddleY < 46) {
-        redPaddleY += 2
+    if (keyName === 'ArrowDown') {
+        redUp = true
     }
 })
+
+addEventListener('keyup', (event) => {
+    keyName = event.key
+    
+    if (keyName === 'w') {
+        blueDown = false
+    }
+    if (keyName === 's') {
+        blueUp = false
+    }
+    if (keyName === 'ArrowUp') {
+        redDown = false
+    }
+    if (keyName === 'ArrowDown') {
+        redUp = false
+    }
+})
+
+let ballX = ((cols * blockSize) / 2)
+let ballY = Math.floor(Math.random() * rows) * blockSize
+let r = blockSize / 2
+let dBallX = 3
+let dBallY = 3
 
 function getPongin() {
     requestAnimationFrame(getPongin)
     c.clearRect(0, 0,rows * blockSize, cols * blockSize)
+
+    if (blueDown === true && bluePaddleY > 0) {
+        bluePaddleY += -1
+    }
+    if (blueUp === true && bluePaddleY < 46) {
+        bluePaddleY += 1
+    }
+    if (redDown === true && redPaddleY > 0) {
+        redPaddleY += -1
+    }
+    if (redUp === true && redPaddleY < 46) {
+        redPaddleY += 1
+    }
     
     c.beginPath()
     c.fillStyle='blue'
@@ -43,5 +83,22 @@ function getPongin() {
         c.fillRect(48 * blockSize, (redPaddleY + i) * blockSize, blockSize, blockSize)
     }
     c.stroke()
+
+    c.beginPath
+    c.fillStyle='black'
+    c.arc(ballX, ballY, r, Math.PI * 2, false)
+    c.stroke()
+
+    if(ballX + r > rows * blockSize || ballX + r < 0) {
+        dBallX = -dBallX
+    }
+    if (ballY + r > cols * blockSize || ballY + r < 0) {
+        dBallY = -dBallY
+    }
+    
+    ballX += dBallX
+    ballY += dBallY
+
+    
 }
 getPongin()
